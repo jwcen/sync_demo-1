931ff9fb1153b073747ab5f5bbe8ddfb429e167b
jayvee <cenjw@m.scnu.edu.cn>
2022-09-04 16:37:59 +0800

    添加静态路由

--- main.go
+++ main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
 	"os/exec"
+	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 func main() {
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		r := gin.Default()
-		
-		r.GET("/", func(ctx *gin.Context) {
-			ctx.Writer.Write([]byte("hello world"))
+
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		r.StaticFS("/static", http.FS(staticFiles))
+		r.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			// 如果路径path是/static开头，就返回首页
+			if strings.HasPrefix(path, "/static") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+
+				stat, err := reader.Stat()  // Statistics返回文件统计信息
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html;charset=utf-8", reader, nil)
+			} else {
+				// 否则404
+				c.Status(http.StatusNotFound)
+			}
 		})
 
 		r.Run(":8080")
 	}()
 
 	chromePath := "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
-	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/")
+	cmd := exec.Command(chromePath, "--app=http://127.0.0.1:8080/static/index.html")
 	cmd.Start()
 
-	select {}
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt)  // Ctrl C 触发中断, 信号写入channel
+
+	// 阻塞，直到接收到信号
+	select {
+	case <-chSignal:
+		cmd.Process.Kill()
+	}
 }
